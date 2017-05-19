ae0e7e8ba7668ebca026c513c5914326c5c0216f
Lasse Fister <commander@graphicore.de>
2017-05-19 13:21:58 +0200

    Use ttfautohint and fontbakery fix-dsig on the ttf files.

--- scripts/bin/build.sh
+++ scripts/bin/build.sh
@@ -4,6 +4,20 @@ cd `dirname $BASH_SOURCE`/../../;
 python -c 'import os;print(os.curdir)'
 . ./venv/bin/activate;
 
+pushd `pwd`;
 cd fonts;
-python -c 'import os;print(os.curdir)'
-fontmake -m ../sources/RobotoSlab.designspace
+fontmake -m ../sources/RobotoSlab.designspace;
+popd;
+
+FILES=$(find fonts/master_ttf -type f -name "*.ttf")
+for f in $FILES;
+do
+    TMP="$(dirname $f)/tmp.ttf"
+    # these arguments are from the initial old/version-1.100263 files
+    # there's a hint in the name-table entry of "version"
+    # made by using the -I option of ttfautohint
+    ttfautohint -l 8 -r 12 -G 200 -x 14 -w "" -W $f $TMP;
+    mv $TMP $f;
+done
+
+fontbakery fix-dsig --autofix $FILES;
