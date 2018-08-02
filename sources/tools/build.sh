b62a1dc9566ce08dfc0c59dba263e1697540a22a
Mike LaGattuta <mjlagattuta@gmail.com>
2018-08-02 11:43:12 -0400

    Added tools and build script

---
+++ sources/tools/build.sh
@@ -0,0 +1,23 @@
+fontmake -o variable -g RobotoSlab.glyphs
+
+rm -rf master_ufo
+
+cd variable_ttf
+
+gftools fix-nonhinting RobotoSlab-VF.ttf RobotoSlab-VF.ttf
+gftools fix-dsig --autofix RobotoSlab-VF.ttf
+gftools fix-gasp RobotoSlab-VF.ttf
+
+ttx RobotoSlab-VF.ttf
+
+rm -rf RobotoSlab-VF.ttf
+rm -rf RobotoSlab-VF-backup-fonttools-prep-gasp.ttf
+
+cd ..
+
+cat variable_ttf/RobotoSlab-VF.ttx | tr '\n' '\r' | sed -e "s,<STAT>.*<\/fvar>,$(cat tools/patch.xml | tr '\n' '\r')," | tr '\r' '\n' > RobotoSlab-VF.ttx
+
+ttx RobotoSlab-VF.ttx
+
+rm -rf RobotoSlab-VF.ttx
+rm -rf variable_ttf
