b540d0e0712ddb18c6edc557dc85dfb18d65cf67
Mike LaGattuta <mjlagattuta@gmail.com>
2018-10-04 14:20:38 -0400

    Updated build script

--- sources/tools/build.sh
+++ sources/tools/build.sh
@@ -20,7 +20,7 @@ rm -rf RobotoSlab-VF-backup-fonttools-prep-gasp.ttf
 
 cd ..
 
-cat variable_ttf/RobotoSlab-VF.ttx | tr '\n' '\r' | sed -e "s,<STAT>.*<\/fvar>,$(cat tools/patch.xml | tr '\n' '\r')," | tr '\r' '\n' > RobotoSlab-VF.ttx
+cat variable_ttf/RobotoSlab-VF.ttx | tr '\n' '\r' | sed -e "s,<STAT>.*<\/STAT>,$(cat tools/patch.xml | tr '\n' '\r')," | tr '\r' '\n' > RobotoSlab-VF.ttx
 
 rm -rf variable_ttf
 
